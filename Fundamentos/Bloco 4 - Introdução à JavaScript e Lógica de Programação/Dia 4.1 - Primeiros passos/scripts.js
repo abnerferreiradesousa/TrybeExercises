f237aba7c1247e42248ffb49bba521b25440c4fa
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2021-10-11 19:58:34 -0500

    Entregando requisito 1 e 2 dos exercicios JavaScript

---
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 4.1 - Primeiros passos/scripts.js
@@ -0,0 +1,22 @@
+//1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados.
+
+// const a = 10;
+// const b = 5;
+// console.log("Soma: " + (a + b));
+// console.log("Subtração: " + (a - b));
+// console.log("Multiplicação: " + (a * b));
+// console.log("Divisão: " + (a / b));
+// console.log("Módulo: " + (a % b));
+
+// 2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const a = 7;
+const b = 10;
+
+if(a > b) {
+    console.log(a);
+} else if (a < b) {
+    console.log(b);
+} else if (a = b)  {
+console.log("São o mesmo valor");
+} 
