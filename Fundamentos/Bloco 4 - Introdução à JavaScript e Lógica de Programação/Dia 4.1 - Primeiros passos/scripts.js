407d355f61a95d762ab33c61b7d2eebd7c605c64
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2021-10-12 07:54:08 -0500

    Requisito 3 verificar de qual o maior entre 3 numeros

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 4.1 - Primeiros passos/scripts.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 4.1 - Primeiros passos/scripts.js
@@ -10,13 +10,30 @@
 
 // 2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 
-const a = 7;
-const b = 10;
+// const a = 7;
+// const b = 10;
 
-if(a > b) {
+// if(a > b) {
+//     console.log(a);
+// } else if (a < b) {
+//     console.log(b);
+// } else if (a == b)  {
+// console.log("São o mesmo valor");
+// } 
+
+// Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+const a = 10;
+const b = 20;
+const c = 30;
+
+if(a > b && a > c) {  
     console.log(a);
-} else if (a < b) {
+} else if(b > a && b > c) { 
     console.log(b);
-} else if (a = b)  {
-console.log("São o mesmo valor");
-} 
+} else if(c > a && c > b) { 
+    console.log(c);
+} else {
+    console.log("São iguais");
+}
+
