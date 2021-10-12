907e3ecdea49fffbcc75a0930b257fd591322559
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2021-10-12 17:28:51 -0500

    Entrega dos requisitos do 1 ao 4 do exercicio de Array e loop For

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 4.2 - Array e loop For/scripts.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 4.2 - Array e loop For/scripts.js
@@ -1,13 +1,60 @@
-// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-// let menuServices = menu[1];
+//for/of
 
-// console.log(menuServices);
+// for(let numero of numbers) {
+//     console.log(numero);
+// }
 
-// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-// let indexOfPortfolio = menu.indexOf('Portifólio');
+// for de sempre
 
-// console.log(indexOfPortfolio);
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push('Contato');
-console.log(menu);
+// 2. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+// MINHA RESOLUÇÃO
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for(let indice = 0; indice < numbers.length; indice += 1) {
+//     soma = soma + numbers[indice];
+// }
+// console.log(soma);
+
+
+//RESOLUÇÃO ALTERNATIVA
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let result = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   result += numbers[index];
+// }
+
+// console.log(result);
+
+// 3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+// let mediaAritmetica = 0;
+
+// for(let indice = 0; indice < numbers.length; indice += 1) {
+//     soma = soma + numbers[indice];
+//     mediaAritmetica = soma / numbers.length;
+// }
+// console.log(mediaAritmetica);
+
+// 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let mediaAritmetica = 0;
+
+for(let indice = 0; indice < numbers.length; indice += 1) {
+    soma = soma + numbers[indice];
+    mediaAritmetica = soma / numbers.length;
+}
+
+if(mediaAritmetica > 20) {
+    console.log("Valor maior que 20");
+} else {
+    console.log("Valor menor ou igual a 20");
+}
