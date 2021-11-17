c1781b04f5ec0e792c4cede75cb99558f02e59b7
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2021-11-17 15:26:07 -0300

    main

--- Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/Dia 7.1 - JavaScript ES6 - let, const, arrow functions e template literals/exer2.js
+++ Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/Dia 7.1 - JavaScript ES6 - let, const, arrow functions e template literals/exer2.js
@@ -1,10 +1,67 @@
-let number = 4;
-let newA = [];
-let result = 0;
+//1.Crie uma função que receba um número e retorne seu fatorial.
 
-for (let index = number; index > 0; index -= 1) {
-    newA.push(index)
-    // console.log(index - 1);
-    let number *= index;
-    console.log(number);
+// let number = 4;
+// let result = 1;
+
+// for (let index = number; index > 0; index -= 1) {
+//     result = result * index;
+// }
+// console.log(result);
+
+//Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
+
+//2.Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+// const largestWord = (text) => {
+//     const  arr = text.split(' ');
+
+//     let result = 0;
+
+//     for (let index = 0; index < arr.length; index += 1) {
+
+//         if(arr[index].length > result){
+//             result = arr[index].length;
+//         }
+//     }
+//     console.log(result);
+// }
+
+// largestWord('Antônio foi no banheiro e não sabemos o que aconteceu')
+
+//PQ MEU RETURN NÃO ESTÁ FUNCIONANDO
+
+
+//3.Crie uma página que contenha:
+
+// const btn = document.querySelector('.btn');
+// const p = document.querySelector('.register')
+
+// let clickCount = 0;
+
+// btn.addEventListener('click', () => {
+//     clickCount += 1; 
+//     p.innerText = `Número de clicks ----> ${clickCount}`;
+//     console.log(clickCount);
+// })
+
+
+//4.Crie um código JavaScript com a seguinte especificação:
+
+//4.1
+
+const findString = (text) => {
+    let strg = "Tryber x aqui!";
+    let newStrg = strg.replace(/x/ig, text); //o 'i' no RegEx serve para ignorar diferenças entre maiúsculas e minúsculas. E o 'g' determina que mais de uma alteração seja feita.
+    // console.log(newStrg);
+}
+
+let word = 'Abner'
+findString(word);
+
+let arr = ['Git', 'HTML', 'EMPATIA', 'JS', 'REDUX'];
+
+//4.2
+
+const recive = (newStrg) => {
+    console.log(newStrg);
 }
---
+++ Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/Dia 8.1 - JavaScript ES6 - Introdução a Higher Order Functions/exer1.js
@@ -0,0 +1,12 @@
+const showName = () => 'Acordando!!';
+// console.log(showName()); 
+
+const chamaCafe = () => 'Bora tomar café!!';
+// console.log(chamaCafe()); 
+
+const vaiDormir = () => 'Partiu dormir!!';
+// console.log(vaiDormir());
+
+const doingThings = (doit) => doit();
+
+console.log(doingThings(chamaCafe));
