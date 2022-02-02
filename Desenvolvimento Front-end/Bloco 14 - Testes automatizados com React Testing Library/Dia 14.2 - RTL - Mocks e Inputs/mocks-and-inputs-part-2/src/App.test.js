e4d5cca45a92a336b7015d45fe464a41d13df076
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-02-01 21:53:56 -0300

    RTL -

---
+++ Desenvolvimento Front-end/Bloco 14 - Testes automatizados com React Testing Library/Dia 14.2 - RTL - Mocks e Inputs/mocks-and-inputs-part-2/src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('Testando inputs', () => {
+
+  test('renders learn react link', () => {
+    render(<App />);
+    //se o input é renderizador
+    //se o input tem como valor ''
+    //se o input tem como valor Estudante...''
+    //input email mesma coisa
+  });
+
+})
