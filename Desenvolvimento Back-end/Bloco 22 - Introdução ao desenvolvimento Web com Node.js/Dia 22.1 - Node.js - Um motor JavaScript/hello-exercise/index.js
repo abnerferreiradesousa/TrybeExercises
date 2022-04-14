23603aa92d0f17e18824464b4e625f1a3b9c58a6
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-04-13 21:52:16 -0300

    bloco 22.2 exercise

---
+++ Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/Dia 22.1 - Node.js - Um motor JavaScript/hello-exercise/index.js
@@ -0,0 +1,23 @@
+// const readline = require('readline-sync');
+
+// const name = readline.question('Qual seu nome? ');
+// const age = readline.questionInt('Qual sua idade? ');
+
+// console.log(`Olá, ${name}! Você tem ${age} anos de idade!`);
+
+// var readlineSync = require('readline-sync');
+ 
+// // Wait for user's response.
+// var userName = readlineSync.question('May I have your name? ');
+// console.log('Hi ' + userName + '!');
+ 
+// // Handle the secret text (e.g. password).
+// var favFood = readlineSync.question('What is your favorite food? ', {
+//   hideEchoBack: false // The typed text on screen is hidden by `*` (default).
+// });
+// console.log('Oh, ' + userName + ' loves ' + favFood + '!');
+
+// var readlineSync = require('readline-sync'),
+//   animals = ['Lion', 'Elephant', 'Crocodile', 'Giraffe', 'Hippo'],
+//   index = readlineSync.keyInSelect(animals, 'Which animal?');
+// console.log('Ok, ' + animals[index] + ' goes to your room.');
