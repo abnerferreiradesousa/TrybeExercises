00a0b06067da974b1a69a785cd7b61903de4258e
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-06-07 15:28:13 -0300

    [feat] type union exercises

--- Desenvolvimento Back-end/Bloco 26 - TypeScript/Dia 26.2 - Tipagem Estática e Generics/ts-26.2/index.ts
+++ Desenvolvimento Back-end/Bloco 26 - TypeScript/Dia 26.2 - Tipagem Estática e Generics/ts-26.2/index.ts
@@ -1,1 +1,29 @@
 let listNames: [string, string] = ['John', 'Abner'];
+
+type address = {
+  street: string;
+  city: string;
+  houseNumber: number;
+  district: string;
+}
+
+type bird = {
+  fly: boolean;
+  eagle: boolean;
+  name: string;
+  specie: string;
+  heigth: number;
+}
+
+enum so {
+  mac,
+  linux,
+}
+
+export function printCPF(cpf:(number | string)) {
+  return console.log(cpf);
+}
+
+export function sum(num1: number, num2: number): number {
+  return num1 + num2
+}
