e906cf4c0507acc4d4282d501bae7c71bda78802
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-06-08 15:58:02 -0300

    [feat] class 26.3 examples

--- Desenvolvimento Back-end/Bloco 26 - TypeScript/Dia 26.2 - Tipagem Estática e Generics/ts-26.2/index.ts
+++ Desenvolvimento Back-end/Bloco 26 - TypeScript/Dia 26.2 - Tipagem Estática e Generics/ts-26.2/index.ts
@@ -27,3 +27,35 @@ export function printCPF(cpf:(number | string)) {
 export function sum(num1: number, num2: number): number {
   return num1 + num2
 }
+
+enum EyeColor {
+  Black = "Pretos",
+  Blue = "Azuis",
+  Green = "Verdes",
+  Brown = "Castanhos",
+}
+
+class Person {
+  name: string;
+  birthDate: Date;
+  eyeColor?: EyeColor;
+
+  constructor(name: string, birthDate: Date, eyeColor?: EyeColor) {
+    this.name = name;
+    this.birthDate = birthDate;
+    this.eyeColor = eyeColor
+  }
+
+  speak(): void {
+    console.log(`${this.name} está falando`);
+  }
+
+  eat(): void {
+    console.log(`${this.name} está comendo`);
+  }
+
+  walk(): void {
+    console.log(`${this.name} está andando`);
+  }
+
+}
